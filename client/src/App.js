d2d046b686e3a4cffab4268d89ef8d1cf5edf2ab
ShayneKaruna <112900215+ShayneKaruna@users.noreply.github.com>
2022-11-25 10:16:02 -0500

    added code to signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,12 @@
-import './App.css';
-
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import {useSelector} from 'react-redux';
-import Home from './components/Home/Home';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-})
+import Register from './Register';
+import Login from './Login';
 
 function App() {
-  const state = useSelector(state => state.UserReducer);
+
   return (
- <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-
-      <Router>
-      
-        <Switch>
-            <Route path="/" exact>
-            {state.user ? <Home/> : <Login/> }
-            </Route>
-            <Route path="/Register">
-                <Register/>
-               
-            </Route>
-        </Switch>
-      </Router>
-
-      </header>
-    </div>
-     </ApolloProvider>
-
+    <main className="App">
+      <Login />
+    </main>
   );
 }
 
