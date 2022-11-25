26fba6a4aec2419f5d811f1c9183b9c389ff3fd0
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-24 22:42:22 -0500

    fixed react render issue

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -6,7 +6,7 @@ import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
-const Signup = () => {
+const Register = () => {
   const [formState, setFormState] = useState({
     username: '',
     email: '',
@@ -97,4 +97,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Register;
