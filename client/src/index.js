b60ae5f226a30f661320123c7e40cec04171c807
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-26 18:13:53 -0500

    redone functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from './App';
-import '../src/App.css'
-import Test from "./testing";
-import allReducers from './reducers'
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import App from './App';
 
-let myStore = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
-  <Provider store={myStore}>
-    <Test />
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
   document.getElementById("root")
 );
