185e28869079e5b293be022add6407567c48480b
jwiz <jordanheath945@gmail.com>
2022-11-26 18:36:40 -0500

    conflictless push

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./pages/register";
+
 import { setContext } from "@apollo/client/link/context";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import "./App.css";
@@ -29,7 +29,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </ApolloProvider>
