3d2b72bcc205a901118e5a142f9e87e44683c22e
ShayneKaruna <112900215+ShayneKaruna@users.noreply.github.com>
2022-11-27 19:01:21 -0500

    added navbar,main page, login and signup

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
-// import Test from './testing';
-// import reportWebVitals from './reportWebVitals';
+import App from './App2';
 
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
-// reportWebVitals();
---
+++ client/src/index2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+// import Test from './testing';
+// import reportWebVitals from './reportWebVitals';
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+// reportWebVitals();
