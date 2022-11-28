b9d7d45c44d00c70f94e172e30e1ff648ee7d344
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-27 21:29:04 -0500

    added next buttons

--- client/src/pages/Content.js
+++ client/src/pages/Content.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function addContent() {
+export default function Content() {
+  const navigate = useNavigate();
   return (
     <div>
       <h2>Please fill out Criteria</h2>
       <div>
         <h4>title:</h4>
-        <input />
+        <input/>
       </div>
       <div>
         <h4>name:</h4>
@@ -36,6 +38,7 @@ export default function addContent() {
         <h4>projects:</h4>
         <input />
       </div>
+      <button onClick={()=>navigate('/finalrender')}>Make Portfolio!</button>
     </div>
   );
 }
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Video from '../videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './styling/HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Portfolio's Made EZ</HeroH1>
+        <HeroP>
+          Use our templates to create the most amazing portfolios.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
