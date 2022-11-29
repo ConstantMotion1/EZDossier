0c3acc138ac73b63f86d29afa0eb5881e453a661
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-29 09:47:59 -0500

    trying a new path

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
+import Footer from '../components/Footer/index';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import {
