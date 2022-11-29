a5a7af6547af7ff9f8142c763d340d6f7c7c48f6
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-29 10:11:47 -0500

    bugfix

--- client/src/pages/FinalPage.js
+++ client/src/pages/FinalPage.js
@@ -30,87 +30,87 @@ export default function FinalPage() {
   const title = postData.title;
   return (
     <>
-     <nav class="navbar">
-        <h1 class="logo">{title}</h1>
-        <ul class="nav-links-container">
-            <li class="nav-link"><a href="#home-section" class="links active">home</a></li>
-            <li class="nav-link"><a href="#project-section" class="links">projects</a></li>
-            <li class="nav-link"><a href="#about-section" class="links">about</a></li>
-            <li class="nav-link"><a href="#contact-section" class="links">contact</a></li>
+     <nav className="navbar">
+        <h1 className="logo">{title}</h1>
+        <ul className="nav-links-container">
+            <li className="nav-link"><a href="#home-section" className="links active">home</a></li>
+            <li className="nav-link"><a href="#project-section" className="links">projects</a></li>
+            <li className="nav-link"><a href="#about-section" className="links">about</a></li>
+            <li className="nav-link"><a href="#contact-section" className="links">contact</a></li>
         </ul>
-        <div class="toggle-btn">
+        <div className="toggle-btn">
             <span></span>
             <span></span>
             <span></span>
         </div>
     </nav>
-    <header class="header" id="home-section">
-        <div class="content">
-            <h1 class="header-heading">Welcome! I'm <span class="brand-name"/>{fullname}</h1>
-            <a href="#" class="btn">{title}</a>
+    <header className="header" id="home-section">
+        <div className="content">
+            <h1 className="header-heading">Welcome! I'm <span className="brand-name"/>{fullname}</h1>
+            <a href="#" className="btn">{title}</a>
         </div>
-        <div class="header-img-container">
-            <div class="circle"></div>
-            <img src={profile} class="header-img" alt=""/>
+        <div className="header-img-container">
+            <div className="circle"></div>
+            <img src={profile} className="header-img" alt=""/>
         </div>
     </header>
     <section id="about-section">
-        <h1 class="heading">about me</h1>
-        <div class="about-container">
-            <div class="image-container">
-                <div class="square"></div>
-                <img src={coding} class="about-img" alt=""/>
-                <div class="social-links">
-                    <a href="#"><img src={github} class="social-img" alt=""/></a>
-                    <a href="#"><img src={insta} class="social-img" alt=""/></a>
-                    <a href="#"><img src={facebook} class="social-img" alt=""/></a>
-                    <a href="#"><img src={twitter} class="social-img" alt=""/></a>
-                    <a href="#"><img src={linkedin} class="social-img" alt=""/></a>
+        <h1 className="heading">about me</h1>
+        <div className="about-container">
+            <div className="image-container">
+                <div className="square"></div>
+                <img src={coding} className="about-img" alt=""/>
+                <div className="social-links">
+                    <a href="#"><img src={github} className="social-img" alt=""/></a>
+                    <a href="#"><img src={insta} className="social-img" alt=""/></a>
+                    <a href="#"><img src={facebook} className="social-img" alt=""/></a>
+                    <a href="#"><img src={twitter} className="social-img" alt=""/></a>
+                    <a href="#"><img src={linkedin} className="social-img" alt=""/></a>
                 </div>
             </div>
-            <div class="info">
+            <div className="info">
                 <p>{description}</p>
-                <a href="#" class="btn">hire me</a>
-                <a href="#" class="btn">download CV</a>
+                <a href="#" className="btn">hire me</a>
+                <a href="#" className="btn">download CV</a>
             </div>
         </div>
     </section>
     <section id="project-section">
-        <h1 class="heading">Projects</h1>
+        <h1 className="heading">Projects</h1>
 
-        <div class="filter">
-            <button class="btn filter-btn active">all</button>
-            <button class="btn filter-btn">css</button>
-            <button class="btn filter-btn">javascript</button>
-            <button class="btn filter-btn">fullstack</button>
+        <div className="filter">
+            <button className="btn filter-btn active">all</button>
+            <button className="btn filter-btn">css</button>
+            <button className="btn filter-btn">javascript</button>
+            <button className="btn filter-btn">fullstack</button>
         </div>
-        <div class="project-container">
-           <div class="project-card">
-                <div class="project-wrapper">
-                    <div class="project-thumbnail">
-                        <img src="img/close.png" class="close-btn" alt=""/>
-                        <img src={project} class="project-img" alt=""/>
-                        <span class="tags">#javascript, #css, #fullstack</span>
+        <div className="project-container">
+           <div className="project-card">
+                <div className="project-wrapper">
+                    <div className="project-thumbnail">
+                        <img src="img/close.png" className="close-btn" alt=""/>
+                        <img src={project} className="project-img" alt=""/>
+                        <span className="tags">#javascript, #css, #fullstack</span>
                     </div>
 
-                    <div class="project-body">
-                        <h1 class="project-name">project 1</h1>
-                        <p class="project-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eum et veniam eos ratione accusantium aut, accusamus deserunt quis dolores.</p>
-                        <a href="#" class="btn">github</a>
-                        <a href="#" class="btn">see live</a>
+                    <div className="project-body">
+                        <h1 className="project-name">project 1</h1>
+                        <p className="project-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eum et veniam eos ratione accusantium aut, accusamus deserunt quis dolores.</p>
+                        <a href="#" className="btn">github</a>
+                        <a href="#" className="btn">see live</a>
                     </div>
                 </div>
             </div> 
         </div>
     </section>
     <section id="contact-section">
-        <h1 class="heading">contact</h1>
-        <div class="form">
+        <h1 className="heading">contact</h1>
+        <div className="form">
             <input type="text" placeholder="name"/>
             <input type="email" placeholder="email"/>
             <input type="text" placeholder="subject"/>
             <textarea placeholder="message"></textarea>
-            <button class="contact-btn">contact</button>
+            <button className="contact-btn">contact</button>
         </div>
     </section>
     </>
