7827aa0eeb726a80199cc2acf26d473125fe5a73
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-29 09:32:30 -0500

    testing deployment

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import {
@@ -26,7 +26,7 @@ function Home() {
       <InfoSection {...homeObjTwo} />
       <Services />
       <InfoSection {...homeObjThree} />
-      <Footer />
+      {/* <Footer/> */}
     </>
   );
 }
